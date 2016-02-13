7b782374c267a73b8dbaacc7de9ec2263276c22d
Luv Saxena <luv.saxena@flipkart.com>
2016-02-13 22:51:01 +0530

    book code created

---
+++ app/controllers/book_controller.rb
@@ -0,0 +1,9 @@
+class BookController < ActionController::API
+  # To change this template use File | Settings | File Templates.
+
+  get '/book/', :provides => [:json] do
+
+      end
+
+
+end
---
+++ app/models/user.rb
@@ -0,0 +1,5 @@
+class User < ActiveRecord::Base
+  #validates :email, :first_name, :last_name, :presence =>true
+  #validates_uniqueness_of :email
+
+end
